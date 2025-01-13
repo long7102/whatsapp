38f4e5960862c442a38d5e47163001c82ef1940f
Nguyễn Việt Long <105055082+long7102@users.noreply.github.com>
2025-01-13 20:28:38 +0700

    Update page.tsx

--- src/app/video-call/page.tsx
+++ src/app/video-call/page.tsx
@@ -4,10 +4,5 @@ import dynamic from "next/dynamic";
 const DynamicVideoUI = dynamic(() => import("./video-ui-kit"), { ssr: false });
 
 export default function VideoCall() {
-  try {
-    return <DynamicVideoUI />;
-  } catch (error) {
-    console.error("Error rendering VideoUI:", error);
-    return <div>Error loading the video call. Please try again.</div>;
-  }
+	return <DynamicVideoUI />;
 }
